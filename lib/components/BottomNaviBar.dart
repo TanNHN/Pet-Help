6ab9249b750c545b584aa96d916850fe454f2e58
anhdung259 <haimaudoimat@gmail.com>
2020-10-11 16:16:25 +0700

    new update bottom navibar

--- lib/components/BottomNaviBar.dart
+++ lib/components/BottomNaviBar.dart
@@ -4,19 +4,48 @@ class BottomNaviBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 80,
-      child: BottomNavigationBar(
-        backgroundColor: Color.fromRGBO(253, 158, 121, 1),
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+      height: 70,
+      child: Scaffold(
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.green,
+          elevation: 5,
+          child: Icon(
+            Icons.search,
           ),
-          BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Post'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle_rounded), label: 'My Pet'),
-        ],
-        selectedItemColor: Colors.white,
+          onPressed: () {},
+        ),
+        bottomNavigationBar: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          color: Color.fromRGBO(253, 158, 121, 1),
+          notchMargin: 7.0,
+          child: Container(
+            height: 70,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.home, color: Colors.white),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(Icons.view_list, color: Colors.white),
+                  onPressed: () {},
+                ),
+                SizedBox(width: 25),
+                IconButton(
+                  icon: Icon(Icons.call, color: Colors.white),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(Icons.person_outline, color: Colors.white),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
