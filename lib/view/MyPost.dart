dfc2fc58a6b2a6b83d3081eb34caea61e2965ef2
Nguyen <sam@google.com>
2020-10-12 19:54:24 +0700

    Update MyPost.dart

--- lib/view/MyPost.dart
+++ lib/view/MyPost.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:pet_help/components/BottomNaviBar.dart';
+// import 'package:pet_help/components/BottomNaviBar.dart';
 import 'package:pet_help/components/listView.dart';
 import 'package:pet_help/components/list_header.dart';
 import 'package:pet_help/view/loadimg.dart';
@@ -20,41 +20,46 @@ class MyPost extends StatelessWidget {
       resizeToAvoidBottomPadding: false,
       appBar: AppBar(
         title: Text(
-          'Post',
+          'POST',
           style: GoogleFonts.lato(
               fontStyle: FontStyle.normal, color: Colors.white, fontSize: 30),
         ),
         centerTitle: true,
         backgroundColor: Color.fromRGBO(253, 158, 121, 1),
-      ),
-      drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            Container(
-              color: Color.fromRGBO(253, 158, 121, 1),
-              height: 200,
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      width: 75,
-                      height: 75,
-                      padding: EdgeInsets.all(15),
-                    ),
-                  ),
-
-                ],
-              ),
-            ),
-          ],
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
         ),
       ),
+
+      // drawer: Drawer(
+      //   // Add a ListView to the drawer. This ensures the user can scroll
+      //   // through the options in the drawer if there isn't enough vertical
+      //   // space to fit everything.
+      //   // child: ListView(
+      //   //   // Important: Remove any padding from the ListView.
+      //   //   padding: EdgeInsets.zero,
+      //   //   children: <Widget>[
+      //   //     // Container(
+      //   //     //   color: Color.fromRGBO(253, 158, 121, 1),
+      //   //     //   height: 200,
+      //   //     //   child: Row(
+      //   //     //     children: <Widget>[
+      //   //     //       Padding(
+      //   //     //         padding: const EdgeInsets.all(8.0),
+      //   //     //         child: Container(
+      //   //     //           width: 75,
+      //   //     //           height: 75,
+      //   //     //           padding: EdgeInsets.all(15),
+      //   //     //         ),
+      //   //     //       ),
+      //   //     //
+      //   //     //     ],
+      //   //     //   ),
+      //   //     // ),
+      //   //   ],
+      //   // ),
+      // ),
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
@@ -65,8 +70,8 @@ class MyPost extends StatelessWidget {
         child: Column(
           children: [
             HeaderList(),
-            Expanded(child: ListViewPet()),
-            BottomNaviBar()
+            Expanded(child: ListViewPet())
+            // ,BottomNaviBar()
           ],
         ),
       ),
