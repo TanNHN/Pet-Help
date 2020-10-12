2a414587a161a2327602d55a7abf1ef01d1dd9a4
Nguyen <sam@google.com>
2020-10-12 20:07:44 +0700

    Nguyen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:pet_help/view/AddPet.dart';
 import 'package:pet_help/view/HomePage.dart';
+import 'package:pet_help/view/MyPet.dart';
 import 'package:pet_help/view/MyPost.dart';
 import 'package:pet_help/view/PetReport.dart';
 import 'package:pet_help/view/findRescue.dart';
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: MyPost (
+        home: MyPet (
             // title: 'Post',
             ));
   }
