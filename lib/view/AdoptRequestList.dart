bc4bbfe3a7af2260e397f445d485eaf2ad611b27
Nguyen <sam@google.com>
2020-10-17 10:06:46 +0700

    Update AdoptRequestList.dart

--- lib/view/AdoptRequestList.dart
+++ lib/view/AdoptRequestList.dart
@@ -11,7 +11,7 @@ class AdoptRequestList extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           leading: IconButton(
-              icon: Icon(Icons.arrow_back, color: Colors.black),
+              icon: Icon(Icons.arrow_back_ios, color: Colors.white),
               onPressed: () {
                 Navigator.of(context).pushReplacement(MaterialPageRoute(
                   builder: (context) => RescueHome(),
