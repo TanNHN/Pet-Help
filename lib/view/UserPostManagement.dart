4bd5d4b7155a93fa38be139a872e2829ad6464f8
lhnguyen99 <lehongnguyen1999@gmail.com>
2020-10-23 19:43:12 +0700

    Update UserPostManagement.dart

--- lib/view/UserPostManagement.dart
+++ lib/view/UserPostManagement.dart
@@ -5,12 +5,14 @@ import 'package:pet_help/components/listView.dart';
 import 'package:pet_help/components/list_header.dart';
 import 'package:pet_help/components/rescue_home_list.dart';
 import 'package:pet_help/view/HomePage.dart';
+import 'package:pet_help/view/MyPostDetail.dart';
 import 'package:pet_help/view/RescueHome.dart';
 import 'package:pet_help/view/loadimg.dart';
 import 'package:pet_help/view/login_page.dart';
 import 'package:pet_help/view/proflie.dart';
 
 import 'AddPet.dart';
+import 'UserPetProfile.dart';
 
 class UserPostManagement extends StatelessWidget {
   final String title;
@@ -77,104 +79,225 @@ class UserPostManagement extends StatelessWidget {
             Expanded(
               child: SingleChildScrollView(
                 child: Column(
-                  children: <Widget>[
-                    Container(
-                      width: double.infinity,
-                      child: Row(children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            children: <Widget>[
-                              Image.asset(
-                                'src/image/rescue_cat3.jpg',
-                                width: 140,
-                                height: 240,
-                              ),
-                            ],
+                  children: [
+                    SizedBox(height: 10),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    new GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                              builder: (context) => new MyPostDetail()),
+                        );
+                      },
+                      child: Container(
+                        width: double.infinity,
+                        child: Row(children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: <Widget>[
+                                Image.asset(
+                                  'src/image/rescue_cat3.jpg',
+                                  width: 140,
+                                  height: 240,
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(8, 19, 8, 8),
-                              child: Container(
-                                width: 220,
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(8, 19, 8, 8),
                                 child: Container(
-                                  width: 180,
+                                  width: 220,
+                                  child: Container(
+                                    width: 180,
+                                    child: (Text(
+                                      "Mèo què cần được vớt...",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    )),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(8, 23, 0, 8),
+                                child: Row(
+                                  children: [
+                                    Text(
+                                      "Được đăng bởi ",
+                                      style: TextStyle(
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      "Tom",
+                                      style: TextStyle(
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.bold,
+                                        color: Color.fromRGBO(253, 158, 121, 1),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+                                child: Container(
+                                  width: 220,
                                   child: (Text(
-                                    "Mèo què cần được vớt...",
+                                    "25 phút trước",
                                     style: TextStyle(
-                                      fontSize: 20,
+                                      color: Colors.grey,
+                                      fontSize: 15,
                                       fontWeight: FontWeight.bold,
                                     ),
                                   )),
                                 ),
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(8, 23, 0, 8),
-                              child: Row(
-                                children: [
-                                  Text(
-                                    "Được đăng bởi ",
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(8, 18, 3, 4),
+                                child: Container(
+                                  width: 220,
+                                  child: (Text(
+                                    "Địa chỉ: 658 Lê Văn Luyện, Q9, tp.HCM",
                                     style: TextStyle(
-                                      fontSize: 17,
+                                      fontSize: 15,
                                       fontWeight: FontWeight.bold,
                                     ),
-                                  ),
-                                  Text(
-                                    "Tom",
-                                    style: TextStyle(
-                                      fontSize: 17,
-                                      fontWeight: FontWeight.bold,
-                                      color: Color.fromRGBO(253, 158, 121, 1),
-                                    ),
-                                  )
-                                ],
+                                  )),
+                                ),
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
-                              child: Container(
-                                width: 220,
-                                child: (Text(
-                                  "25 phút trước",
-                                  style: TextStyle(
-                                    color: Colors.grey,
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                )),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(8, 18, 3, 4),
-                              child: Container(
-                                width: 220,
-                                child: (Text(
-                                  "Địa chỉ: 658 Lê Văn Luyện, Q9, tp.HCM",
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                )),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ]),
-                      height: 200,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(15),
-                          boxShadow: [
-                            new BoxShadow(
-                                color: Colors.black54,
-                                offset: new Offset(1.0, 2.0),
-                                blurRadius: 3.5),
-                          ]),
+                            ],
+                          ),
+                        ]),
+                        height: 200,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [
+                              new BoxShadow(
+                                  color: Colors.black54,
+                                  offset: new Offset(1.0, 2.0),
+                                  blurRadius: 3.5),
+                            ]),
+                      ),
                     ),
+                    SizedBox(height: 10),
+                    new GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                              builder: (context) => new MyPostDetail()),
+                        );
+                      },
+                      child: Container(
+                        width: double.infinity,
+                        child: Row(children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              children: <Widget>[
+                                Image.asset(
+                                  'src/image/rescue_cat3.jpg',
+                                  width: 140,
+                                  height: 240,
+                                ),
+                              ],
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(8, 19, 8, 8),
+                                child: Container(
+                                  width: 220,
+                                  child: Container(
+                                    width: 180,
+                                    child: (Text(
+                                      "Mèo què cần được vớt...",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    )),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(8, 23, 0, 8),
+                                child: Row(
+                                  children: [
+                                    Text(
+                                      "Được đăng bởi ",
+                                      style: TextStyle(
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Text(
+                                      "Tom",
+                                      style: TextStyle(
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.bold,
+                                        color: Color.fromRGBO(253, 158, 121, 1),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+                                child: Container(
+                                  width: 220,
+                                  child: (Text(
+                                    "25 phút trước",
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  )),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(8, 18, 3, 4),
+                                child: Container(
+                                  width: 220,
+                                  child: (Text(
+                                    "Địa chỉ: 658 Lê Văn Luyện, Q9, tp.HCM",
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  )),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ]),
+                        height: 200,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [
+                              new BoxShadow(
+                                  color: Colors.black54,
+                                  offset: new Offset(1.0, 2.0),
+                                  blurRadius: 3.5),
+                            ]),
+                      ),
+                    ),
+                    SizedBox(height: 10),
                   ],
                 ),
               ),
