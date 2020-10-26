556ee16f490fa201c645262319ea78fa128bf34e
lhnguyen99 <lehongnguyen1999@gmail.com>
2020-10-26 13:41:09 +0700

    Nguyen update

--- lib/view/Login.dart
+++ lib/view/Login.dart
@@ -18,6 +18,36 @@ class LoginState extends State<Login> {
   var userInvalid = false;
   var passInvalid = false;
 
+  Widget _signInButton() {
+    return OutlineButton(
+      splashColor: Colors.grey,
+      onPressed: () {},
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      highlightElevation: 0,
+      borderSide: BorderSide(color: Colors.grey),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(image: AssetImage("src/icon/google_logo.png"), height: 35.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign in with Google',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -75,6 +105,28 @@ class LoginState extends State<Login> {
                   ),
                 ),
               ),
+              _signInButton(),
+              // Padding(
+              //   padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+              //   child: Row(
+              //     mainAxisSize: MainAxisSize.min,
+              //     mainAxisAlignment: MainAxisAlignment.center,
+              //     children: <Widget>[
+              //       Image(image: AssetImage("src/icon/google_logo.png"), height: 35.0),
+              //       Padding(
+              //         padding: const EdgeInsets.only(left: 10),
+              //         child: Text(
+              //           'Sign in with Google',
+              //           style: TextStyle(
+              //             fontSize: 20,
+              //             color: Colors.grey,
+              //           ),
+              //         ),
+              //       )
+              //     ],
+              //   ),
+              // ),
+
               ResponsiveGridRow(
                 children: [
                   ResponsiveGridCol(
@@ -111,6 +163,7 @@ class LoginState extends State<Login> {
       ),
     );
   }
+
 
   void onSignInClick() {
     setState(() {
