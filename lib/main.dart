ff137e65ae72ed941d334efa2cc0f418201018db
lhnguyen99 <lehongnguyen1999@gmail.com>
2020-10-28 13:54:41 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,6 @@ import 'package:pet_help/view/Login.dart';
 void main() {
   runApp(MaterialApp(
    home: Login(),
-   debugShowCheckedModeBanner: false,
+   // debugShowCheckedModeBanner: false,
   ));
 }
