74eb81e9b43fa82fcc088a117cd9525b159807ea
lhnguyen99 <lehongnguyen1999@gmail.com>
2020-10-28 10:58:52 +0700

    Update rescue_home_list.dart

--- lib/components/rescue_home_list.dart
+++ lib/components/rescue_home_list.dart
@@ -74,7 +74,7 @@ class RescueHomeList extends StatelessWidget {
                               ),
                             ),
                             Container(
-                              width: size.width * 0.3,
+                              // width: size.width * 0.3,
                               child: Column(
                                 children: [
                                   Text(
@@ -219,7 +219,7 @@ class RescueHomeList extends StatelessWidget {
                               ),
                             ),
                             Container(
-                              width: size.width * 0.3,
+                              // width: size.width * 0.3,
                               child: Column(
                                 children: [
                                   Text(
@@ -364,7 +364,7 @@ class RescueHomeList extends StatelessWidget {
                               ),
                             ),
                             Container(
-                              width: size.width * 0.3,
+                              // width: size.width * 0.3,
                               child: Column(
                                 children: [
                                   Text(
@@ -453,6 +453,153 @@ class RescueHomeList extends StatelessWidget {
                     ]),
               ),
             ),
+            SizedBox(height: 10),
+            SizedBox(height: 10),
+            new GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  new MaterialPageRoute(
+                      builder: (context) => new UserPostDetail()),
+                );
+              },
+              child: Container(
+                width: double.infinity,
+                child: Row(children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: <Widget>[
+                        Image.asset(
+                          'src/image/rescue_cat2.jpg',
+                          width: size.width * 0.45,
+                          height: size.height * 0.44,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(8, 19, 8, 8),
+                        child: Container(
+                          width: size.width * 0.35,
+                          child: Container(
+                            child: (Text(
+                              "Chú mèo bị bỏ rơi",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(1, 23, 0, 8),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: size.width * 0.1,
+                              child: Column(
+                                children: [
+                                  Icon(Icons.account_circle_outlined),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              // width: size.width * 0.3,
+                              child: Column(
+                                children: [
+                                  Text(
+                                    "Nguyen",
+                                    style: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color.fromRGBO(253, 158, 121, 1),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+                        child: Container(
+                          width: size.width * 0.4,
+                          child: (Text(
+                            "15 ngày trước",
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(1, 23, 0, 8),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: size.width * 0.1,
+                              child: Column(
+                                children: [
+                                  Icon(Icons.location_on),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              width: size.width * 0.33,
+                              child: Column(
+                                children: [
+                                  Text(
+                                    "658 Quang Trung, Q12, TP.HCM",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+                        child: Container(
+                          width: size.width * 0.4,
+                          child: (Text(
+                            "Còn cách nhau 20 km",
+                            style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )),
+                        ),
+                      ),
+                    ],
+                  ),
+                ]),
+                height: size.height * 0.4,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      new BoxShadow(
+                          color: Colors.black54,
+                          offset: new Offset(1.0, 2.0),
+                          blurRadius: 3.5),
+                    ]),
+              ),
+            ),
+            SizedBox(height: 10),
+            SizedBox(height: 10),
           ],
         ),
       ),
