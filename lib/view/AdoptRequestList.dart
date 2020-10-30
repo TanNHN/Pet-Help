9204d26f1a0def132e5edbbed5976094290c4606
lhnguyen99 <lehongnguyen1999@gmail.com>
2020-10-30 12:48:28 +0700

    Update AdoptRequestList

--- lib/view/AdoptRequestList.dart
+++ lib/view/AdoptRequestList.dart
@@ -71,7 +71,7 @@ class AdoptRequestList extends StatelessWidget {
                               children: [
                                 Icon(Icons.pets),
                                 Text(
-                                  "Tên pet: ",
+                                  " Tên pet: ",
                                   style: TextStyle(
                                     fontSize: 15,
                                     fontWeight: FontWeight.bold,
@@ -94,7 +94,7 @@ class AdoptRequestList extends StatelessWidget {
                               children: [
                                 Icon(Icons.person),
                                 Text(
-                                  "Người nhận nuôi: ",
+                                  " Người nhận nuôi: ",
                                   style: TextStyle(
                                     fontSize: 15,
                                     fontWeight: FontWeight.bold,
@@ -117,7 +117,7 @@ class AdoptRequestList extends StatelessWidget {
                               children: [
                                 Icon(Icons.date_range),
                                 Text(
-                                  "Ngày: ",
+                                  " Ngày: ",
                                   textAlign: TextAlign.left,
                                   style: TextStyle(
                                     fontSize: 15,
@@ -136,18 +136,42 @@ class AdoptRequestList extends StatelessWidget {
                             ),
                           ),
                           Padding(
-                            padding: const EdgeInsets.fromLTRB(8, 0, 3, 4),
-                            child: Container(
-                              width: 220,
-                              child: Text(
-                                "Địa chỉ: 658 Lê Văn Luyện, Q9, tp.HCM",
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.bold,
+                            padding: const EdgeInsets.fromLTRB(8, 0, 0, 8),
+                            child: Row(
+                              children: [
+                                Icon(Icons.home),
+                                Text(
+                                  " Địa chỉ: ",
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                              ),
+                                Text(
+                                  " 658 Lê Văn Luyện, Q9",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color.fromRGBO(253, 158, 121, 1),
+                                  ),
+                                )
+                              ],
                             ),
                           ),
+                          // Padding(
+                          //   padding: const EdgeInsets.fromLTRB(8, 0, 3, 4),
+                          //   child: Container(
+                          //     width: 220,
+                          //     child: Text(
+                          //       "Địa chỉ: 658 Lê Văn Luyện, Q9, tp.HCM",
+                          //       style: TextStyle(
+                          //         fontSize: 15,
+                          //         fontWeight: FontWeight.bold,
+                          //       ),
+                          //     ),
+                          //   ),
+                          // ),
                         ],
                       ),
                     ]),
@@ -253,18 +277,42 @@ class AdoptRequestList extends StatelessWidget {
                           ),
                         ),
                         Padding(
-                          padding: const EdgeInsets.fromLTRB(8, 0, 3, 4),
-                          child: Container(
-                            width: 220,
-                            child: Text(
-                              "a a a a a a a a a a a a a a a a a a a ",
-                              style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold,
+                          padding: const EdgeInsets.fromLTRB(8, 0, 0, 8),
+                          child: Row(
+                            children: [
+                              Icon(Icons.home),
+                              Text(
+                                " Địa chỉ: ",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                            ),
+                              Text(
+                                " 658 Lê Văn Luyện, Q9",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromRGBO(253, 158, 121, 1),
+                                ),
+                              )
+                            ],
                           ),
                         ),
+                        // Padding(
+                        //   padding: const EdgeInsets.fromLTRB(8, 0, 3, 4),
+                        //   child: Container(
+                        //     width: 220,
+                        //     child: Text(
+                        //       "a a a a a a a a a a a a a a a a a a a ",
+                        //       style: TextStyle(
+                        //         fontSize: 15,
+                        //         fontWeight: FontWeight.bold,
+                        //       ),
+                        //     ),
+                        //   ),
+                        // ),
                       ],
                     ),
                   ]),
@@ -280,6 +328,145 @@ class AdoptRequestList extends StatelessWidget {
                       ]),
                 ),
                 SizedBox(height: 10),
+                Container(
+                  width: double.infinity,
+                  child: Row(children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: <Widget>[
+                          Image.asset(
+                            'src/image/cat6.jpg',
+                            width: 90,
+                            height: 90,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(8, 8, 8, 8),
+                          child: Row(
+                            children: [
+                              Icon(Icons.pets),
+                              Text(
+                                "Tên pet: ",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                "Ngáo",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromRGBO(253, 158, 121, 1),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(8, 0, 0, 8),
+                          child: Row(
+                            children: [
+                              Icon(Icons.person),
+                              Text(
+                                "Người nhận nuôi: ",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                "Nguyen",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromRGBO(253, 158, 121, 1),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(8, 0, 0, 8),
+                          child: Row(
+                            children: [
+                              Icon(Icons.date_range),
+                              Text(
+                                "Ngày: ",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                "26/9/2020",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromRGBO(253, 158, 121, 1),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(8, 0, 0, 8),
+                          child: Row(
+                            children: [
+                              Icon(Icons.home),
+                              Text(
+                                " Địa chỉ: ",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                " 658 Lê Văn Luyện, Q9",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromRGBO(253, 158, 121, 1),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        // Padding(
+                        //   padding: const EdgeInsets.fromLTRB(8, 0, 3, 4),
+                        //   child: Container(
+                        //     width: 220,
+                        //     child: Text(
+                        //       "a a a a a a a a a a a a a a a a a a a ",
+                        //       style: TextStyle(
+                        //         fontSize: 15,
+                        //         fontWeight: FontWeight.bold,
+                        //       ),
+                        //     ),
+                        //   ),
+                        // ),
+                      ],
+                    ),
+                  ]),
+                  height: 150,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        new BoxShadow(
+                            color: Colors.black54,
+                            offset: new Offset(1.0, 2.0),
+                            blurRadius: 3.5),
+                      ]),
+                ),
               ],
             ),
           ),
