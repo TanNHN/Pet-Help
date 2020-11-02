a67e217cf6fe67b80f64b3ad97c7909925642776
lhnguyen99 <lehongnguyen1999@gmail.com>
2020-11-02 15:30:39 +0700

    Update data and json

--- lib/Model/Pet.dart
+++ lib/Model/Pet.dart
@@ -3,13 +3,16 @@ class Pet {
   final String address;
   final String user;
   final String pathImg;
+  final String distance;
 
-  Pet({this.title, this.address, this.user, this.pathImg});
+  Pet({this.title, this.address, this.user, this.pathImg, this.distance});
   factory Pet.fromJson(Map<String, dynamic> json) {
     return Pet(
         title: json['title'],
         address: json['address'],
         user: json['user'],
-        pathImg: json['pathImg']);
+        pathImg: json['pathImg'],
+        distance: json['distance']
+    );
   }
 }
